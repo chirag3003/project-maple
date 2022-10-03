04eb481189cadbe30448f04df78b255071ee7be9
chirag bhalotia <me@chirag.codes>
2022-10-03 15:50:58 +0530

    dashboard status setup

--- src/packages/frontend/components/Dashboard/index.jsx
+++ src/packages/frontend/components/Dashboard/index.jsx
@@ -1,7 +1,7 @@
 import { LineChart } from "@tremor/react";
 import React from "react";
 import DashboardStyle from "./Dashboard.style";
-const chartdata = [
+const cpuData = [
     {
         time: "10:00",
         "Cpu Usage": 1.74,
@@ -23,6 +23,29 @@ const chartdata = [
         "Cpu Usage": 1,
     },
 ];
+
+const ramData = [
+    {
+        time: "10:00",
+        "Ram Usage": 1.74,
+    },
+    {
+        time: "12:00",
+        "Ram Usage": 1.93,
+    },
+    {
+        time: "14:00",
+        "Ram Usage": 1.9,
+    },
+    {
+        time: "16:00",
+        "Ram Usage": 1.98,
+    },
+    {
+        time: "18:00",
+        "Ram Usage": 1,
+    },
+];
 const dataFormatter = (number) => `${Intl.NumberFormat("us").format(number).toString()}%`;
 
 function Dashboard() {
@@ -30,12 +53,37 @@ function Dashboard() {
         <DashboardStyle>
             <div className="row ">
                 <div className="w-1/2 pr-5">
-                    <div className="card w-full shadow h-full min-h-min bg-gradient-to-br from-green-200 to-green-500"></div>
+                    <div className="card w-full shadow h-full min-h-min ">
+                        <div className="name">
+                            <h2 className="text-2xl font-bold">Chirag{"'"}s Server</h2>
+                            <ul className="mt-2 text-xl stats-list font-medium">
+                                <li>
+                                    <span>OS version: </span> <span>Ubuntu 20.2</span>{" "}
+                                </li>
+                                <li>
+                                    <span>Maple Version:</span>
+                                    <span>20.2</span>
+                                </li>
+                                <li>
+                                    <span>CPU Usage:</span>
+                                    <span> 20.2%</span>
+                                </li>
+                                <li>
+                                    <span>RAM Usage: </span>
+                                    <span>12%</span>
+                                </li>
+                                <li>
+                                    <span>Storage Usage:</span>
+                                    <span> 43% </span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
-                <div className="w-1/2 pr-5 ">
+                {/* <div className="w-1/2 pr-5 ">
                     <div className="card w-full shadow h-full min-h-min">
                         <LineChart
-                            data={chartdata}
+                            data={cpuData}
                             dataKey="time"
                             categories={["Cpu Usage"]}
                             colors={["green"]}
@@ -43,16 +91,25 @@ function Dashboard() {
                             yAxisWidth="w-10"
                         />
                     </div>
-                </div>
+                </div> */}
             </div>
-            <div className="row ">
+            {/* <div className="row ">
                 <div className="w-1/2 pr-5">
-                    <div className="card w-full shadow h-full min-h-min"></div>
+                    <div className="card w-full shadow h-full min-h-min">
+                        <LineChart
+                            data={ramData}
+                            dataKey="time"
+                            categories={["Ram Usage"]}
+                            colors={["blue"]}
+                            valueFormatter={dataFormatter}
+                            yAxisWidth="w-10"
+                        />
+                    </div>
                 </div>
                 <div className="w-1/2 pr-5 ">
                     <div className="card  w-full shadow h-full min-h-min"></div>
                 </div>
-            </div>
+            </div> */}
         </DashboardStyle>
     );
 }
