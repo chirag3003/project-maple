78e94b354d06a15aff49364be1375a4a399f972e
its_GKRam <itsgkram@gmail.com>
2022-10-14 15:26:59 +0530

    Z index Problem resolved in Root layout Sidebar Menu

--- src/packages/frontend/components/layouts/RootLayout/RootLayout.index.jsx
+++ src/packages/frontend/components/layouts/RootLayout/RootLayout.index.jsx
@@ -3,11 +3,11 @@ import RootLayoutBody from './Body/Body.index';
 import RootLayoutSidebar from './Sidebar/Sidebar.index';
 
 export default function RootLayout({ children }) {
-    const [isOpen, setIsOpen] = React.useState(false);
+    const [isOpen, setIsOpen] = React.useState(true);
     return (
         <div className='relative flex flex-col w-screen h-screen lg:flex-row '>
             <div
-                className={`sticky transition-all lg:h-full w-full  ${
+                className={`sticky z-50 transition-all lg:h-full w-full  ${
                     isOpen ? 'lg:w-[90px]' : 'h-full lg:w-1/6 '
                 }`}
             >
@@ -19,7 +19,7 @@ export default function RootLayout({ children }) {
                 />
             </div>
             <div
-                className={`w-full h-full overflow-hidden  ${
+                className={`w-full  h-full overflow-hidden  ${
                     !isOpen &&
                     'hidden lg:block transition-all lg:w-5/6 lg:h-full'
                 }`}
